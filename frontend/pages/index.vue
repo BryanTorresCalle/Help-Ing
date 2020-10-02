9b263bc16eafcc2091ef7de9cf0fb19e6b3dbae8
BryanTorresCalle <50812831+BryanTorresCalle@users.noreply.github.com>
2020-10-01 22:18:23 -0500

    "Arreglo de index"

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="container">
-    <div>
+    <div class="title">
       <h1>Help-Ing: Contenido Universal</h1>
+      
     </div>
+    <div class="link"><a href="insert_uc" >Agregar Contenido Universal </a></div>
+    <div class="link"><a href="list_uc">Listar Contenido Universal</a></div>
+    
 
 
 
@@ -12,4 +16,18 @@
 <script>
 export default {}
 </script>
+
+<style>
+.link {
+  font-size: 30px;
+  margin: 75px;
+  text-align: center;
+  
+  
+}
+
+.title{
+  text-align: center;
+}
+</style>
 
